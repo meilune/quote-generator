9d5d3d89bb7a1ef294e2f6dfca610ff805f09d4c
Ginja <virginija.meilune@gmail.com>
2022-08-02 06:27:52 +1000

    Adding Javascript for quote extraction

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
-console.log("Testing")
+async function getQuotes() {
+    fetch("https://type.fit/api/quotes")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            const dataResponse = data[Math.floor(Math.random() * data.length)]
+            console.log(dataResponse);
+        });
+}
+
+//On load
+getQuotes();
