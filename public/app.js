1bfa546ded0043ac2a1d08a6c4b8434c0a7faf2a
Ginja <virginija.meilune@gmail.com>
2022-08-02 14:12:04 +1000

    Commiting JS code for code generation and female oriented quotes file

--- public/app.js
+++ public/app.js
@@ -1,13 +1,31 @@
+// const apiURL = "https://type.fit/api/quotes";
+const apiURL = "uploads/quotes.json"
+const quoteText = document.getElementById("quote-text");
+const quoteAuthor = document.getElementById("quote-author");
+
 async function getQuotes() {
-    fetch("https://type.fit/api/quotes")
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            const dataResponse = data[Math.floor(Math.random() * data.length)]
-            console.log(dataResponse);
-        });
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    const dataResponse = data[Math.floor(Math.random() * data.length)]; 
+    quoteText.textContent += dataResponse.text;
+    if (dataResponse.author === null) {
+        quoteAuthor.textContent += "Anonymous";
+    } else {
+        quoteAuthor.textContent += dataResponse.author;
+    };
 }
 
 //On load
-getQuotes();
+getQuotes()
+.catch(error => {
+    console.error(error);
+});
+
+function refreshPage(){
+    window.location.reload();
+} 
+// function loadTweet() {
+//     twitterLink = "https://twitter.com/intent/tweet?text="+quoteText+" By "+quoteAuthor;
+//     document.getElementById("twitter-id").href = twitterLink;
+// }
+
