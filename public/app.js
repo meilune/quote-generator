6aa7420b61b1bfe868a3279cf0182a37c95155a7
Ginja <virginija.meilune@gmail.com>
2022-08-02 16:09:03 +1000

    Adding final code touches and styling

--- public/app.js
+++ public/app.js
@@ -1,9 +1,31 @@
 // const apiURL = "https://type.fit/api/quotes";
-const apiURL = "uploads/quotes.json"
+const apiURL = "https://meilune.github.io/quote-generator/uploads/quotes.json"
 const quoteText = document.getElementById("quote-text");
 const quoteAuthor = document.getElementById("quote-author");
+const twitterBtn = document.getElementById("twitter");
+const load = document.getElementById("loader");
+const quoteContainer = document.getElementById("quote-container");
+
+//loading
+function loading() {
+    load.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//hide loading
+function complete() {
+    load.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+//On load
+getQuotes()
+.catch(error => {
+    console.error(error);
+});
 
 async function getQuotes() {
+    loading();
     const response = await fetch(apiURL);
     const data = await response.json();
     const dataResponse = data[Math.floor(Math.random() * data.length)]; 
@@ -13,19 +35,18 @@ async function getQuotes() {
     } else {
         quoteAuthor.textContent += dataResponse.author;
     };
+    complete();
 }
 
-//On load
-getQuotes()
-.catch(error => {
-    console.error(error);
-});
+
 
 function refreshPage(){
+    loading();
     window.location.reload();
 } 
-// function loadTweet() {
-//     twitterLink = "https://twitter.com/intent/tweet?text="+quoteText+" By "+quoteAuthor;
-//     document.getElementById("twitter-id").href = twitterLink;
-// }
+//Tweeting a quote
+function loadTweet() {
+    const twitterLink = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+    window.open(twitterLink, '_blank');
+}
 
